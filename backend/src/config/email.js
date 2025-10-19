584ddb905923c9845ebbb381e2171fd8f6cce7a5
Junior2601 <plj63376@gmail.com>
2025-10-19 21:37:45 +0300

    trust

--- backend/src/config/email.js
+++ backend/src/config/email.js
@@ -7,7 +7,7 @@ dotenv.config();
 export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,   // ex: "smtp.gmail.com"
   port: process.env.SMTP_PORT,   // ex: 587
-  secure: false,                 // true si 465
+  secure: true,                 // true si 465
   auth: {
     user: process.env.SMTP_USER, // adresse email
     pass: process.env.SMTP_PASS  // mot de passe / app password
